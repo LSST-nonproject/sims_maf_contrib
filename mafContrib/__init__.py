b8e5c7fde860eefa97f7f0191260d41155740730
Phil Marshall <dr.phil.marshall@gmail.com>
2014-08-16 23:37:42 -0700

    Season stacker, plus summary metrics for the lensed quasar time delays driver

--- mafContrib/__init__.py
+++ mafContrib/__init__.py
@@ -1,3 +1,10 @@
 from exampleNewMetrics import *
 from exampleNewStacker import *
-# Add similar lines (from filename import *) when you add new metrics, stackers or slicers.
+# Add similar lines (from filename import *) when you add new metrics, 
+# stackers or slicers.
+#
+# Phil Marshall, drphilmarshall, <pjm@slac.stanford.edu>
+from seasonStacker import *
+from seasonLengthMetric import *
+from campaignLengthMetric import *
+from meanNightSeparationMetric import *
