2686c69b5cbeeb8949c08b125f3b7cde34ac65ab
Simon Krughoff <simon.krughoff@gmail.com>
2014-09-30 15:59:06 -0700

    Added docstrings for the variability metric.

--- mafContrib/varMetrics.py
+++ mafContrib/varMetrics.py
@@ -1,8 +1,23 @@
+# Example of a *very* simple variabiilty metric
+# krughoff@uw.edu
+
 from lsst.sims.maf.metrics import BaseMetric
 import numpy
 from scipy.signal import lombscargle
 
 def find_period(times, mags, minperiod=2., maxperiod=35., nbins=1000):
+    """
+    Find the period of a lightcurve.  The parameters used here imply magnitudes
+    but there is no reason this would not work if fluxes are passed.
+
+    :param times: A list of times for the given observations
+    :param mags: A list of magnitudes for the object at the given times
+    :param minperiod: Minimum period to search
+    :param maxperiod: Maximum period to search
+    :param nbins: Number of frequency bins to use in the search
+    :returns: Period in the same units as used in times.  This is simply
+              the max value in the Lomb-Scargle periodogram
+    """
     # Recenter the magnitude measurements about zero
     dmags = mags - numpy.median(mags)
 
@@ -17,7 +32,21 @@ def find_period(times, mags, minperiod=2., maxperiod=35., nbins=1000):
     return 1./f[idx]
 
 class SinPeriodMetric(BaseMetric):
+    """
+    Use pure sine wave variabiility (in magnitude) to probe the ability to recover
+    variability of various periods.
+    """
+
     def __init__(self, col, periodMin=3., periodMax=35., meanMag=21., amplitude=1., **kwargs):
+        """
+        Consltruct an instance of a SinPeriodMetric class
+
+        :param col: Name of the column to use for the observation times, commonly 'expMJD'
+        :param periodMin: Minimum period to test (days)
+        :param periodMax: Maximimum period to test (days)
+        :param meanMag: Mean value of the lightcurve
+        :param amplitude: Amplitude of the variation (mags)
+        """
         self.periodMin = periodMin
         self.periodMax = periodMax
         self.meanMag = meanMag
@@ -25,6 +54,13 @@ class SinPeriodMetric(BaseMetric):
         super(SinPeriodMetric, self).__init__(col, **kwargs)
 
     def run(self, dataSlice, slicePoint):
+        """
+        Run the SinPeriodMetric
+        :param dataSlice: Data for this slice.
+        :param slicePoint: Metadata for the slice.
+        :return: The period estimated from a Lomb-Scargle periodogram
+        """
+
         # Make sure the observation times are sorted
         data = numpy.sort(dataSlice[self.colname])
 
