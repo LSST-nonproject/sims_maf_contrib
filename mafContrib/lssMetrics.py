724e102b18bdbd7c1bcf527b3587e9ce1fc4ece5
Lynne Jones <ljones.uw@gmail.com>
2015-02-17 16:17:06 -0800

    Updated scaling for galaxy number counts.

--- mafContrib/lssMetrics.py
+++ mafContrib/lssMetrics.py
@@ -16,7 +16,7 @@ class GalaxyCountsMetric(BaseMetric):
         self.coaddmetric = Coaddm5Metric(m5Col=self.m5Col)
         # Total of 41253.0 galaxies across the sky (at what magnitude?).
         # This didn't seem to work quite right for me..
-        self.scale = 41253.0 / hp.nside2npix(nside)
+        self.scale = 41253.0 / hp.nside2npix(nside) / 5000.
 
     def _galCount(self, coaddm5, apparent_mag):
         dn_gal = np.power(10., -3.52) * np.power(10., 0.34*apparent_mag)
