490eb7b6a6f33b63668e95c8a5f9f506e79cd422
Lynne Jones <lynnej@uw.edu>
2015-09-22 12:06:21 -0700

    Cleaning up duplicate/replacement code. Use new seasonStacker as it fixed a bug in the old version.

--- mafContrib/seasonStacker.py
+++ mafContrib/seasonStacker.py
@@ -1,22 +1,24 @@
-# ======================================================================
-# Phil Marshall (drphilmarshall) <pjm@slac.stanford.edu>
-# ======================================================================
+#####################################################################################
+# Modified version of Phill Mashall's Season Stacker
+# Humna Awan: humna.awan@rutgers.edu
+# Date last modified: 07/28/15
+#####################################################################################
 
 import numpy as np
 from lsst.sims.maf.stackers import BaseStacker
 
 rad2deg = 180.0/np.pi
 
-class SeasonStacker(BaseStacker):
+class SeasonStacker_v2(BaseStacker):
     """
     Add an integer label to show which season a given visit is in.
     The season only depends on the RA of the object: we compute the MJD
     when each object is on the meridian at midnight, and subtract 6 
     months to get the start date of each season.
         
-    Units: none (the season is just an integer label)
+    The season index range is 0-10. 
+    Must wrap 0th and 10th to get a total of 10 seasons.
     
-    Used by: LensedQuasarTimeDelays, ...
     """
     def __init__(self, expMJDCol='expMJD',RACol='fieldRA'):
         # Names of columns we want to add.
@@ -34,34 +36,25 @@ class SeasonStacker(BaseStacker):
         simData = self._addStackers(simData)
         # Define year number:
         year = np.floor((simData[self.expMJDCol] - simData[self.expMJDCol][0]) / 365.25)
-        # BUG: the offset should be by the survey start date, not
-        # the first observation of this field...
+
+        objRA= simData[self.RACol]*rad2deg/15.0   # in hrs
+            
+        # objRA=0 on autumnal equinox.
+        # autumnal equinox 2014 happened on Sept 23 --> Equinox MJD
+        Equinox = 2456923.5 - 2400000.5
+
+        daysSinceEquinox = 0.5*objRA*(365.25/12.0)  # 0.5 to go from RA to month; 365.25/12.0 for months to days
+        firstSeasonBegan = Equinox + daysSinceEquinox - 0.5*365.25   # in MJD
         
-        # Define season by finding date at which this object's RA is
-        # overhead at the middle of the night, and then checking 6
-        # months either side. First get RA of Sun, when object is at mid
-        # point of its season.
-        sunRA = simData[self.RACol]*rad2deg/15.0 - 12.0
-        sunRA[np.where(sunRA < 0.0)] += 24.0
-        # The Sun is at this RA when N months have passed since March
-        # 21, where N = 0.5*SunRA. Let's work out the first date when
-        # this happened after March 21, 2014, and then subtract half a
-        # year to get the MJD when this first season started.
-        # Modified Julian Date at midnight on March 21st, 2014 was
-        Equinox = 2456737.5 - 2400000.5
-        daysSinceEquinox = 0.5*sunRA*(365.25/12.0)
-        firstSeasonBegan = Equinox + daysSinceEquinox - 0.5*365.25
         # Now we can compute the number of years since the first season 
         # began, and so assign a global integer season number:
         globalSeason = np.floor((simData[self.expMJDCol] - firstSeasonBegan)/365.25)
         # Subtract off season number of first observation:
         season = globalSeason - np.min(globalSeason) 
-        # Done!
         
         simData['year'] = year
         simData['season'] = season
 
         return simData
 
-# ======================================================================
-
+###############################################################################################
