da9a1baf8de3b087d91797991478a2dee6e767b1
Humna <humna.awan@rutgers.edu>
2016-06-27 02:33:52 -0400

    Added new optional inputs; if only mask is involved, the corresponding data is not zeroed out anymore; pixelRadius=0 case now handled separately.

--- mafContrib/maskingAlgorithmGeneralized.py
+++ mafContrib/maskingAlgorithmGeneralized.py
@@ -1,46 +1,51 @@
 #####################################################################################################
-# Motivation: changing the values/mask of a metricBundle in the pixels with a
-# certain value/mask. Example applicaiton: mask the outermost edge of skymaps. 
+# Purpose: change the values/mask of a metricBundle in the pixels with a certain value/mask.
+# Example applicaton: mask the outermost/shallow edge of skymaps. 
 
 # Humna Awan: humna.awan@rutgers.edu
-# Date last modified: 08/18/15
+# Date last modified: 06/10/16
 #####################################################################################################
 
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 import healpy as hp
 import copy
 import lsst.sims.maf.plots as plots
+import matplotlib.cm as cm
 
 __all__ = ['maskingAlgorithmGeneralized']
-
 
 def maskingAlgorithmGeneralized(myBundles, plotHandler, dataLabel, nside= 128,
                                 findValue= 'unmasked', relation= '=',
                                 newValue= 'masked',
                                 pixelRadius= 6, returnBorderIndices= False,
-                                printInfo= True, plotIntermediatePlots= True,
-                                skyMapColorMin= -0.12, skyMapColorMax= 0.12):
+                                printIntermediateInfo= False, plotIntermediatePlots= True,
+                                printFinalInfo= True,
+                                plotFinalPlots= True,
+                                skyMapColorMin= None, skyMapColorMax= None):
     """
     Assign newValue to all pixels in a skymap within pixelRadius of pixels with value <, >, or = findValue.
 
-    Required arguments:
-      * myBundles -  a dictionary for metricBundles.
-      * plotHandler - lsst.sims.maf.plots.plotHandler.PlotHandler object for skymaps and power spectra
-      * dataLabel - a string describing that date, i.e. 'numGal'
+    Required Parameters
+    --------------------
+      * myBundles: a dictionary for metricBundles.
+      * plotHandler: lsst.sims.maf.plots.plotHandler.PlotHandler object for skymaps and power spectra
+      * dataLabel: str: description of the data, i.e. 'numGal'
 
-    Pre-Set/optional arguments:
-      * nside - HEALpix resolution parameter. default value: 128
-      * findValue - if related to mask, must be either 'masked' or 'unmasked'. otherwise, must be a number. default: 'unmasked'
-      * relation - must be '>','=','<'. default: '='
-      * newValue - if related to mask, must be either 'masked' or 'unmasked'. otherwise, must be a number. default: 'masked'
-      * pixelRadius - number of pixels to consider around a given pixel. default: 6
-      * returnBorderIndices - set to True to return the array of indices of the pixels whose values/mask are changed. default: False
-      * printInfo - set to False if do not want to print intermediate info. default: True
-      * plotIntermediatePlots - set to False if do not want to plot intermediate plots. default: True
-      * skyMapColorMin - colorMin label value for skymap plotDict label. default: -0.12
-      * skyMapColorMax - colorMax label value for skymap plotDict label. default: 0.12
+    Optional arguments
+    -------------------
+      * nside: int: HEALpix resolution parameter. Default: 128
+      * findValue: if related to mask, must be either 'masked' or 'unmasked'. otherwise, must be a number. Default: 'unmasked'
+      * relation: str: must be '>','=','<'. Default: '='
+      * newValue: if related to mask, must be either 'masked' or 'unmasked'; otherwise, must be a number. Default: 'masked'
+      * pixelRadius: int: number of pixels to consider around a given pixel. Default: 6
+      * returnBorderIndices: boolean: set to True to return the array of indices of the pixels whose values/mask are changed. Default: False
+      * printIntermediateInfo: boolean: set to False if do not want to print intermediate info. Default: True
+      * plotIntermediatePlots: boolean: set to False if do not want to plot intermediate plots. Default: True
+      * printFinalInfo: boolean: set to False if do not want to print final info, i.e. total pixels changed. Default: True
+      * plotFinalPlots: boolean: set to False if do not want to plot the final plots. Default: True
+      * skyMapColorMin: float: colorMin label value for skymap plotDict label. Default: None
+      * skyMapColorMax: float: colorMax label value for skymap plotDict label. Default: None
 
     """
     # find pixels such that (pixelValue (relation) findValue) AND their neighbors dont have that (relation) findValue.
@@ -48,6 +53,17 @@ def maskingAlgorithmGeneralized(myBundles, plotHandler, dataLabel, nside= 128,
     # relation must be '>','=','<'
     
     # data indices are the pixels numbers ..
+
+    if (pixelRadius == 0):
+        print 'No masking/changing of the original data.'
+        if returnBorderIndices:
+            borders= {}
+            for dither in myBundles:
+                borders[dither]= []
+                
+            return [myBundles, borders]
+        else:
+            return myBundles
     
     # make sure that relation is compatible with findValue
     if ((findValue == 'masked') | (findValue == 'unmasked')):
@@ -78,6 +94,9 @@ def maskingAlgorithmGeneralized(myBundles, plotHandler, dataLabel, nside= 128,
     for dither in myBundles:
         totalBorderPixel= []
 
+        if printIntermediateInfo:
+            print 'Survey strategy: ', dither
+            
         # find the appropriate array to look at.
         if (str(findValue)).__contains__('mask'):
             origArray= myBundles[dither].metricValues.mask.copy()    # mask array
@@ -115,7 +134,7 @@ def maskingAlgorithmGeneralized(myBundles, plotHandler, dataLabel, nside= 128,
             borderPixel= np.unique(borderPixel)
             totalBorderPixel.extend(borderPixel)
 
-            if printInfo:
+            if printIntermediateInfo:     
                 print 'Border pixels from run', r+1, ':', len(borderPixel)
                 print 'Total pixels so far: ', len(totalBorderPixel)
                 print ''
@@ -127,13 +146,13 @@ def maskingAlgorithmGeneralized(myBundles, plotHandler, dataLabel, nside= 128,
                     tempCopy[dither].metricValues.mask[totalBorderPixel]= not(newValueToAssign)
                     tempCopy[dither].metricValues.data[totalBorderPixel]= -500
                     plotDict = {'xlabel': dataLabel, 'title':'%s: %s Round # %s' %(dither, dataLabel, r+1), 
-                                'logScale': False, 'labelsize': 9,'colorMin':-550, 'colorMax': 550}
+                                'logScale': False, 'labelsize': 9,'colorMin':-550, 'colorMax': 550, 'cmap':cm.jet}
                 else:
                     tempCopy[dither].metricValues.mask[:]= True
                     tempCopy[dither].metricValues.mask[totalBorderPixel]= False
                     tempCopy[dither].metricValues.data[totalBorderPixel]= newValueToAssign
                     plotDict = {'xlabel': dataLabel, 'title':'%s %s Round # %s' %(dither, dataLabel, r+1), 
-                                'logScale': False, 'labelsize': 9, 'maxl': 500}
+                                'logScale': False, 'labelsize': 9, 'maxl': 500, 'cmap':cm.jet}
                 tempCopy[dither].setPlotDict(plotDict)
                 tempCopy[dither].setPlotFuncs([plots.HealpixSkyMap(), plots.HealpixPowerSpectrum()])
                 tempCopy[dither].plot(plotHandler=plotHandler)
@@ -144,26 +163,31 @@ def maskingAlgorithmGeneralized(myBundles, plotHandler, dataLabel, nside= 128,
     # change the original map/array.
     for dither in myBundles:
         totalBorderPixel= borders[dither]
-        
+
+        if printFinalInfo:
+            print 'Survey strategy: ', dither
+            print 'Total pixels changed: ', len(totalBorderPixel)
+            print ''
+            
         if (str(newValue)).__contains__('mask'):
             myBundles[dither].metricValues.mask[totalBorderPixel]= newValueToAssign
-            myBundles[dither].metricValues.data[totalBorderPixel]= 0.0
         else:
             myBundles[dither].metricValues.data[totalBorderPixel]= newValueToAssign
             
-        plotDict = {'xlabel':dataLabel, 'title':'%s: %s MaskedMap; pixelRadius: %s ' %(dither,dataLabel, pixelRadius), 
-                    'logScale': False, 'labelsize': 8,'colorMin': skyMapColorMin, 'colorMax': skyMapColorMax}
-        myBundles[dither].setPlotDict(plotDict)
-        myBundles[dither].setPlotFuncs([plots.HealpixSkyMap()])
-        myBundles[dither].plot(plotHandler=plotHandler)
-        
-        plotDict = {'xlabel':dataLabel, 'title':'%s: %s MaskedMap; pixelRadius: %s ' %(dither,dataLabel, pixelRadius), 
-                    'logScale': False, 'labelsize': 12, 'maxl': 500}
-        myBundles[dither].setPlotDict(plotDict)
-        myBundles[dither].setPlotFuncs([plots.HealpixPowerSpectrum()])
-        myBundles[dither].plot(plotHandler=plotHandler)
-
-        plt.show()
+        if plotFinalPlots:
+            plotDict = {'xlabel':dataLabel, 'title':'%s: %s MaskedMap; pixelRadius: %s ' %(dither,dataLabel, pixelRadius), 
+                        'logScale': False, 'labelsize': 8,'colorMin': skyMapColorMin, 'colorMax': skyMapColorMax, 'cmap':cm.jet}
+            myBundles[dither].setPlotDict(plotDict)
+            myBundles[dither].setPlotFuncs([plots.HealpixSkyMap()])
+            myBundles[dither].plot(plotHandler=plotHandler)
+            
+            plotDict = {'xlabel':dataLabel, 'title':'%s: %s MaskedMap; pixelRadius: %s ' %(dither,dataLabel, pixelRadius), 
+                        'logScale': False, 'labelsize': 12, 'maxl': 500, 'cmap':cm.jet}
+            myBundles[dither].setPlotDict(plotDict)
+            myBundles[dither].setPlotFuncs([plots.HealpixPowerSpectrum()])
+            myBundles[dither].plot(plotHandler=plotHandler)
+            
+            plt.show()
 
     if returnBorderIndices:
         return [myBundles, borders]
