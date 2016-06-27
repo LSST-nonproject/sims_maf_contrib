d5a2652efa2c9723352c648ff3dc1b78313ec347
Humna <humna.awan@rutgers.edu>
2016-06-27 04:29:05 -0400

    Metric to calculate number of observations in a given data slice.

---
+++ mafContrib/numObsMetric.py
@@ -0,0 +1,27 @@
+#####################################################################################################
+# Purpose: Calculate the number of observations in a given dataslice.
+
+# Humna Awan: humna.awan@rutgers.edu
+# Last updated: 06/10/15
+ #####################################################################################################
+ 
+from lsst.sims.maf.metrics import BaseMetric
+
+__all__= ['NumObsMetric']
+
+class NumObsMetric(BaseMetric):
+    """
+    Calculate the number of observations per data slice, e.g. HealPix pixel when using HealPix slicer.
+
+    Optional Parameters:
+    --------------------
+    * fieldIDCol: str: Name of the fieldID column in the data. Default: 'fieldID'.
+    * nside: int: HEALpix resolution parameter. Default: 128
+
+    """
+    def __init__(self, fieldIDCol = 'fieldID', nside=128, metricName='NumObsMetric', **kwargs):
+        self.fieldIDCol = fieldIDCol
+        super(NumObsMetric, self).__init__(col=self.fieldIDCol, metricName=metricName, **kwargs)
+
+    def run(self, dataSlice, slicePoint=None):
+        return len(dataSlice)
