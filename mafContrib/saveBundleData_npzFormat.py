bc1fe00727c452c0476874e3f90c4d318ec1eda7
Humna <humna.awan@rutgers.edu>
2016-06-28 00:59:55 -0400

    Method to save data from a metricBundle object as .npz files.

---
+++ mafContrib/saveBundleData_npzFormat.py
@@ -0,0 +1,37 @@
+#####################################################################################################
+# Purpose: save data from a metricBundle object as .npz files.
+
+# Humna Awan: humna.awan@rutgers.edu
+# Last updated: 06/27/15
+ #####################################################################################################
+
+import os
+
+__all__ = ['saveBundleData_npzFormat']
+
+
+def saveBundleData_npzFormat(path, bundle, baseFilename, filterBand):
+     """
+
+     Save data in the metricBundle. For each key, a new .npz file is created to
+     save the content of the metricBundle object.
+     
+     Required Parameters
+     -------------------
+       * path: str: path to the directory where the files should be saved
+       * bundle: metricBundle object whose contents are to be saved.
+       * baseFilename: str: basic filename wanted. Final filename would be
+                            baseFilename + '_' + filterBand + '_' + dither + '.npz'
+       * filterBand: str: filter of the data in the bundle, e.g. 'r'
+  
+    """   
+     for dither in bundle:
+         outfile = baseFilename + '_' + filterBand + '_' + dither + '.npz'
+         bundle[dither].slicer.writeData(os.path.join(path, outfile),
+                                         bundle[dither].metricValues,
+                                         metricName = bundle[dither].metric.name,
+                                         simDataName = bundle[dither].runName,
+                                         constraint = bundle[dither].constraint,
+                                         metadata = bundle[dither].metadata,
+                                         displayDict = bundle[dither].displayDict,
+                                         plotDict = bundle[dither].plotDict)
