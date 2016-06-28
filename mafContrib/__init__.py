93728d332914a4ee5dfd4426ec7422f7994adc6e
Humna <humna.awan@rutgers.edu>
2016-06-28 01:13:12 -0400

    Added lines for plotBundleMaps, saveBundleData_npzFormat, numObsMetric, galaxyCountsMetric_extended, galaxyCounts_withPixelCalibration, artificialStructureCalculation, almPlots, and coaddM5Analysis.

--- mafContrib/__init__.py
+++ mafContrib/__init__.py
@@ -26,3 +26,16 @@ from .newDitherStackers import *
 from .maskingAlgorithmGeneralized import *
 from .periodicStarMetric import *
 from .GRBTransientMetric import *
+
+# Humna:
+from .plotBundleMaps import *
+from .saveBundleData_npzFormat import *
+
+from .numObsMetric import *
+from .galaxyCountsMetric_extended import *
+from .galaxyCounts_withPixelCalibration import *
+from .artificialStructureCalculation import *
+
+from .almPlots import *
+from .coaddM5Analysis import *
+
