b2fb88dfeaf28da5c6b0eaa86872cb642726ad87
Humna <humna.awan@rutgers.edu>
2016-08-18 15:01:34 -0700

    Added constantsForPipeline.

--- mafContrib/__init__.py
+++ mafContrib/__init__.py
@@ -30,12 +30,11 @@ from .GRBTransientMetric import *
 # Humna:
 from .plotBundleMaps import *
 from .saveBundleData_npzFormat import *
-
 from .numObsMetric import *
 from .galaxyCountsMetric_extended import *
 from .galaxyCounts_withPixelCalibration import *
 from .artificialStructureCalculation import *
-
 from .almPlots import *
 from .coaddM5Analysis import *
 
+from .constantsForPipeline import *
