08ac7f35d6fd269df149caeb98ff2584ae23507f
Peter Yoachim <yoachim@uw.edu>
2019-07-08 11:47:25 -0700

    fix init for LSS stuff

--- mafContrib/LSSObsStrategy/__init__.py
+++ mafContrib/LSSObsStrategy/__init__.py
@@ -1,14 +1,13 @@
-__all__= [
-        'newDitherStackers',
-        'maskingAlgorithmGeneralized',
-        'plotBundleMaps',
-        'saveBundleData_npzFormat',
-        'numObsMetric',
-        'galaxyCountsMetric_extended',
-        'galaxyCounts_withPixelCalibration',
-        'artificialStructureCalculation',
-        'almPlots',
-        'coaddM5Analysis',
-        'constantsForPipeline',
-        'os_bias_analysis'
-]
+from .newDitherStackers import *
+from .newDitherStackers import *
+from .maskingAlgorithmGeneralized import *
+from .plotBundleMaps import *
+from .saveBundleData_npzFormat import *
+from .numObsMetric import *
+from .galaxyCountsMetric_extended import *
+from .galaxyCounts_withPixelCalibration import *
+from .artificialStructureCalculation import *
+from .almPlots import *
+from .coaddM5Analysis import *
+from .constantsForPipeline import *
+from .os_bias_analysis import *
