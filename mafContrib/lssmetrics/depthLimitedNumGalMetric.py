91cd05d65fd69805ea6817f0c3c0c41059d89398
Lynne Jones <lynnej@uw.edu>
2020-07-24 22:09:11 -0700

    Made maps inheritance easier/more consistent

--- mafContrib/lssmetrics/depthLimitedNumGalMetric.py
+++ mafContrib/lssmetrics/depthLimitedNumGalMetric.py
@@ -5,7 +5,6 @@ from mafContrib.LSSObsStrategy.galaxyCountsMetric_extended import GalaxyCountsMe
 
 class DepthLimitedNumGalMetric(metrics.BaseMetric):
     """
-
     This metric calculates the number of galaxies while accounting for the extragalactic footprint.
 
     Parameters
@@ -35,18 +34,19 @@ class DepthLimitedNumGalMetric(metrics.BaseMetric):
     Returns
     -------
     Number of galaxies in healpix if the slicePoint passes the extragalactic cuts; otherwise self.badval
-
     """
     def __init__(self, m5Col='fiveSigmaDepth', filterCol='filter',
-                 maps=['DustMap'], nside=128, filterBand='i', redshiftBin='all',
-                 nfilters_needed=6, lim_mag_i_ptsrc=26.0, lim_ebv=0.2, **kwargs):
+                 nside=128, filterBand='i', redshiftBin='all',
+                 nfilters_needed=6, lim_mag_i_ptsrc=26.0, lim_ebv=0.2,
+                 metricName='DepthLimitedNumGalaxiesMetric', units='Galaxy Counts', **kwargs):
         self.m5Col = m5Col
         self.filterCol = filterCol
         self.filterBand = filterBand
         # set up the extended source limiting mag
         # galaxies are x2 as seeing: seeing is generally 0.7arcsec and a typical galaxies is 1arcsec
-        # => for extended source limiting mag of x, we'd need x + 0.7 as the point-source limiting mag; 0.7 comes from
-        # $\sqrt{1/2}$; basically have x2 difference in magnitudes between point source and extended source.
+        # => for extended source limiting mag of x, we'd need x + 0.7 as the point-source limiting mag;
+        # 0.7 comes from $\sqrt{1/2}$;
+        # basically have x2 difference in magnitudes between point source and extended source.
         lim_mag_i_extsrc = lim_mag_i_ptsrc - 0.7
         # set up the metric for galaxy counts
         self.galmetric = GalaxyCountsMetric(m5Col=self.m5Col, nside=nside,
@@ -54,13 +54,16 @@ class DepthLimitedNumGalMetric(metrics.BaseMetric):
                                             includeDustExtinction=True,
                                             filterBand=self.filterBand, redshiftBin=redshiftBin,
                                             CFHTLSCounts=False,
-                                            normalizedMockCatalogCounts=True,
-                                            maps=maps)
+                                            normalizedMockCatalogCounts=True)
         # set up the metric for extragalactic footprint
         self.eg_metric = metrics.ExgalM5_with_cuts(m5Col=self.m5Col, filterCol=self.filterCol,
                                                    lsstFilter=self.filterBand, nFilters=nfilters_needed,
                                                    extinction_cut=lim_ebv, depth_cut=lim_mag_i_ptsrc)
-        super().__init__(col=[self.m5Col, self.filterCol], maps=maps, **kwargs)
+        maps = self.eg_metric.maps + self.galmetric.maps
+        maps = set(maps)
+        super().__init__(col=[self.m5Col, self.filterCol], maps=maps,
+                         metricName=metricName, units=units,
+                         **kwargs)
 
     def run(self, dataslice, slicePoint):
         # see if this slicePoint is in the extragalactic footprint
