58c7ea8ee2ecea9706ab17d33c6103ed31f7f336
LSST-sersag <78701856+LSST-sersag@users.noreply.github.com>
2021-11-23 01:48:52 +0100

    Add files via upload

--- mafContrib/AGN_TimeLagMetric.py
+++ mafContrib/AGN_TimeLagMetric.py
@@ -1,85 +1,91 @@
-import rubin_sim.maf as maf
-import os
 import numpy as np
-import math
+# from .baseMetric import BaseMetric
+import rubin_sim.maf as maf
+from rubin_sim.photUtils import Dust_values
 
-class AGN_TimeLagMetric(maf.BaseMetric):
-    lag = 100
-    z = 0
-    name = 'AGN_TimeLag_Metric'
-    
-    # Different way of calculating nquist. Default value is 'mean', therefore meaon value of sampling time is used.
-    calcType = 'mean'
-    
-    # If Log scale is used. Default value is False
-    log = False
+__all__ = ['AGN_TimeLagMetric']
 
-    # Calculate NQUIST value for time-lag and sampling time (redshift is included in formula if desired)
-    def __getNquistValue(self, caden, lag, z):
-        return (lag/((1+z)*caden))
-    
-    def setLag(self, lag):
-        self.lag = lag
-    
-    def setRedshift(self, z):
-        self.z = z
-    
-    def setName(self, name):
-        self.name = name
-    
-    
-    def __init__(self, lag, name = 'AGN_TimeLag_Metric', z=1, log = False):
-       
-        cols = ['observationStartMJD', 'filter' ] 
-        
+
+class AGN_TimeLagMetric(BaseMetric):
+    def __init__(self, lag=100, z=1, log=False, threshold=2.2, calcType='mean',
+                 mjdCol='observationStartMJD', m5Col='fiveSigmaDepth', filterCol='filter', dust = True,
+                 metricName=None, **kwargs):
         self.lag = lag
         self.z = z
         self.log = log
-        self.name = name
-        super().__init__(col=cols, metricName=name,  metricDtype='float')
+        self.threshold = threshold
+        self.calcType = calcType
+        self.mjdCol = mjdCol
+        self.filterCol = filterCol
+        self.dust = dust
+        self.m5Col = m5Col
+        if metricName is None:
+            metricName = f'AGN_TimeLag_{lag}_days'
+            
+        if dust:
+            maps = ['DustMap']
+            dust_properties = Dust_values()
+            self.Ax1 = dust_properties.Ax1
+        else:
+            maps = []
         
-    
+        super().__init__(col=[self.mjdCol, self.filterCol, self.m5Col], metricName=metricName, **kwargs)
+
+    # Calculate NQUIST value for time-lag and sampling time (redshift is included in formula if desired)
+    def _getNquistValue(self, caden, lag, z):
+        return (lag / ((1 + z) * caden))
+
     def run(self, dataSlice, slicePoint=None):
         
-        mv = np.sort(dataSlice["observationStartMJD"])
+        #Dust extinction
+        if self.dust:
+            new_m5 = dataSlice[self.m5Col]*0
+            for filtername in np.unique(dataSlice[self.filterCol]):
+                in_filt = np.where(dataSlice[self.filterCol] == filtername)[0]
+                A_x = self.Ax1[filtername]
+                new_m5[in_filt] = dataSlice[self.m5Col][in_filt] - A_x
+                
+                
+            dataSlice[self.m5Col] = new_m5
+            
+        mjds = []
+        for el in dataSlice:
+            if( (el[self.m5Col] < 22.0 and el[self.filterCol] == 'g') or ( el[self.m5Col] < 21.8 and el[self.filterCol] == 'r') ) :
+                mjds.append(el[self.mjdCol])
+            if( el[self.filterCol] in ['u', 'i', 'z'] ):
+                mjds.append(el[self.mjdCol])
+                
+       
+        # Calculate differences in time between visits
+        mv = np.sort(mjds)
         val = np.diff(mv)
-        
-        
-        if self.calcType == 'mean':
-            val = (np.mean(val))
-        elif self.calcType == 'min':
-            if len(val)  == 0:
-                val = np.nan
-            else:
-                val = (np.min(val))
-        elif self.calcType == 'max':
-            if len(val) == 0:
-                val = np.nan
-            else:
-                val = np.max(val)
-        else:
-            #gcd 
-            val = np.rint(val)
-            val = val.astype(int)
-            val = np.gcd.reduce(val)
-          
-        if  math.isnan(val) :
-                nquist = np.nan
-        else:
-            if self.log == True:
-                nquist = np.log(self.__getNquistValue(val, self.lag, self.z))
-            else:
-                nquist = self.__getNquistValue(val, self.lag, self.z) 
+        # If there was only one visit; bail out now.
+        if len(val) == 0:
+            return self.badval
 
-        result = nquist
-        
-        # Threshold nquist value is 2.2, hence we are aiming to show values higher than threshold (2.2) value
-        if self.log == True:
-            if nquist < np.log(2.2):
-                result = np.nan
+        # Otherwise summarize the time differences as:
+        if self.calcType == 'mean':
+            val = np.mean(val)
+        elif self.calcType == 'min':
+            val = np.min(val)
+        elif self.calcType == 'max':
+            val = np.max(val)
         else:
-            if nquist<2.2:
-                result = np.nan
-        
-        
-        return result
+            # find the greatest common divisor
+            val = np.rint(val).astype(int)
+            val = np.gcd.reduce(val)
+
+        # Will always have a value at this point
+        nquist = self._getNquistValue(val, self.lag, self.z)
+        if self.log:
+            nquist = np.log(nquist)
+
+        # Threshold nquist value is 2.2,
+        # hence we are aiming to show values higher than threshold (2.2) value
+        threshold = self.threshold
+        if self.log:
+            threshold = np.log(threshold)
+
+        if nquist < threshold:
+            nquist = self.badval
+        return nquist
